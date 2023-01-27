a07a752dbef51beeedc6659512d8aaf385fb9aab
kaaf7 <amer.m.karim@gmail.com>
2023-01-27 11:39:23 +0100

    Edit comment typos

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@
  */
 
 const mongoose = require("mongoose");
-// create new shcema
+// create new schema
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
